1ba794d4fdbe5bb04a11ff5c977fd455192ebb5e
Aitor Santin <santinaitor@gmail.com>
2022-06-18 10:42:31 +0200

    Cambios de diseño

--- ProgramaPorteros/SeguimientoDeAcciones.Designer.cs
+++ ProgramaPorteros/SeguimientoDeAcciones.Designer.cs
@@ -28,20 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // Form1
+            // label1
+            // 
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SEGUIMIENTO DE ACCIONES";
+            // 
+            // SeguimientoDeAcciones
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Blue;
             this.ClientSize = new System.Drawing.Size(1432, 805);
-            this.Name = "Form1";
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "SeguimientoDeAcciones";
             this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.White;
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
 
